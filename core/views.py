b79cf71c4b1d5c210925b03dc9bbd15af95d3217
Grom <egor.maksimovich.s@gmail.com>
2026-07-21 09:49:06 +0300

    Создан прототип страницы со списком объектов

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import Object
 
-# Create your views here.
+
+def is_master(user):
+    return user.groups.filter(name='master').exists()
+
+
+@login_required
+@user_passes_test(is_master, login_url='/access-denied/')
+def master_dashboard(request):
+    """Главная страница для мастера со списком всех объектов"""
+    objects_list = Object.objects.select_related('status', 'client').all()
+
+    context = {
+        'objects': objects_list,
+    }
+    return render(request, 'master_dashboard.html', context)
